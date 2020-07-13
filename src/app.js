5e99aaa7c1f2b393a319924fd2292677906906b6
Ihsaan <homeosx@Gotens-Macbook=Air.local>
2020-07-12 23:55:35 -0500

    first commit

---
+++ src/app.js
@@ -0,0 +1,36 @@
+var electron = require('electron');
+var app = electron.app;
+var BrowserWindow = electron.BrowserWindow;
+var mainWindow = null;
+var ipc = require('ipc');
+
+app.on('ready', function() {
+    mainWindow = new BrowserWindow({
+        width: 1024,
+        height: 768
+    });
+
+    mainWindow.loadURL('file://' + __dirname + '/windows/main/main.html');
+    mainWindow.openDevTools();
+});
+
+function createInsertWindow() {
+    insertWindow = new BrowserWindow({
+        width: 640,
+        height: 480,
+        show: false
+    });
+    insertWindow.loadURL('file://' + __dirname + '/windows/insert/insert.html')
+
+    insertWindow.on('closed', function() {
+        insertWindow = null;
+    });
+
+    ipc.on('toggle-insert-view', function() {
+        if (!insertWindow) {
+            createInsertWindow();
+        }
+        return (!insertWindow.isClosed() && insertWindow.isVisible()) ? insertWindow.hide() : insertWindow.show();
+    });
+
+}
